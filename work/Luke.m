a2c4f5f01e5b8f328b4de1de6718d530e79bca4c
VeronicaBirova <101700968+VeronicaBirova@users.noreply.github.com>
2023-02-05 18:18:59 +0300

    Create Luke.m

---
+++ work/Luke.m
@@ -0,0 +1,58 @@
+function Luke
+  pkg load symbolic;
+  clear;  clc;
+  p=[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 91, 97, 101, 103, 107, 109, 113, 127];
+  k=randi([5,10]);
+  a(1)=2;
+  deg(1)=20;
+
+  for i=2:1:k
+    a(i)=p(randi([1,32]));
+    deg(i)=randi([18,25]);
+  end
+  el=max(a);
+  n=0;
+  for i=1:1:k
+    n=n+a(i)^deg(i);
+  end
+
+  while((n>2^128)&&(n<2^123))
+    k=randi([5,10]);
+    a(1)=2;
+    deg(1)=20;
+    for i=2:1:k
+        a(i)=p(randi([1,32]));
+        deg(i)=randi([18,25]);
+    end
+    el=max(arr);
+    n=0;
+    for i=1:1:k
+      n=n+a(i)^deg(i);
+    end
+    log2(n);
+  end
+
+  n++;  s=0;
+  %ceil(x) - округление к ближайшему целому, не меньшему, чем x
+  for c=2:1:ceil(log2(n))
+    if(mod(c^(n-1),n)==1)
+        ans=1;
+        for i=1:1:k
+            if(mod(c^((n-1)/p(i)),n)==1)
+                ans=0;
+            end
+        end
+        if(ans==1)
+            s=a;
+        end
+    end
+  end
+
+  if(s!=0)
+    disp(sym(n))
+    disp("This number is prime");
+  else
+   disp(sym(n));
+   disp("This number isn't prime");
+  end
+end
