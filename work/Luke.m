f3140bf947e59afa4ab63e3bdb800e32db86cdd9
VeronicaBirova <101700968+VeronicaBirova@users.noreply.github.com>
2023-02-07 06:30:20 +0300

    Update Luke.m

--- work/Luke.m
+++ work/Luke.m
@@ -6,49 +6,48 @@ function Luke
   a(1)=2;
   deg(1)=20;
 
-  for i=2:1:k
+  for i=2:k
     a(i)=p(randi([1,32]));
     deg(i)=randi([18,25]);
   end
-  el=max(a);
-  n=0;
-  for i=1:1:k
-    n=n+a(i)^deg(i);
+  el=max(a); n=0;
+  for i=1:k
+    n+=a(i)^deg(i);
   end
 
   while((n>2^128)&&(n<2^123))
     k=randi([5,10]);
     a(1)=2;
     deg(1)=20;
-    for i=2:1:k
+    for i=2:k
         a(i)=p(randi([1,32]));
         deg(i)=randi([18,25]);
     end
     el=max(arr);
     n=0;
-    for i=1:1:k
+    for i=1:k
       n=n+a(i)^deg(i);
     end
-    log2(n);
+    %log2(n); %вычисляем логарифм по основанию 2 для x
   end
 
-  n++;  s=0;
+  n++; 
   %ceil(x) - округление к ближайшему целому, не меньшему, чем x
-  for c=2:1:ceil(log2(n))
+  for c=2:ceil(log2(n))
     if(mod(c^(n-1),n)==1)
-        ans=1;
-        for i=1:1:k
+        f=1;
+        for i=1:k
             if(mod(c^((n-1)/p(i)),n)==1)
-                ans=0;
+                f=0;
             end
         end
-        if(ans==1)
-            s=a;
+        if(f==1)
+            t=a;
         end
     end
   end
 
-  if(s!=0)
+  if(t!=0)
     disp(sym(n))
     disp("This number is prime");
   else
