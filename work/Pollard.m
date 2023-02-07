343eb280589401a369ea8f5e05badbb66499bbfa
VeronicaBirova <101700968+VeronicaBirova@users.noreply.github.com>
2023-02-07 06:14:29 +0300

    Update Pollard.m

--- work/Pollard.m
+++ work/Pollard.m
@@ -2,20 +2,27 @@ function Pollard
   clear;  clc;
   n=input('Enter a composite number: ');
   p=2;  i=2;  k=1;
-  d(1)=gcd(p-1,n); %наибольший общий делитель
-  d1(k)=n/d(k);
+
+  d0(1)=gcd(p-1,n); %наибольший общий делитель
+  d1(k)=n/d0(k);
   while(gcd(p-1,n)==1)
     p=rem(p^i,n); %остаток от деления p^i на n
     i++;
-    d(k)=gcd(p-1,n);
-    d1(k)=n/d(k);
-    if((isprime(d1(k))==true)&&(isprime(d(k))==true))
-      break;
-    elseif((isprime(d(k))==true)&&(isprime(d1(k))==false))
+    d0(k)=gcd(p-1,n);
+    d1(k)=n/d0(k);
+    if((isprime(d0(k))==true)&&(isprime(d1(k))==true))
+      disp(d0)
+      disp(d1)
+    elseif((isprime(d0(k))==true)&&(isprime(d1(k))==false))%||...
+           %((isprime(d0(k))==false)&&(isprime(d1(k))==true)))%||...
+           %((isprime(d0(k))==false)&&(isprime(d1(k))==false)))
         n=d1(k);
-        k++;  p=2;  i=2;
+        p=2;  i=2;  k++;
+        %disp(d0)
+        %disp(d1)
     end
   end
-  disp(d)
-  disp(d1)
+  %disp(d0)
+  %disp(d1)
 end
+
