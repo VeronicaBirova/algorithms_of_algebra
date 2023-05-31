7a190dfed9589cd1d2599174b3cabddf76b4b6b7
VeronicaBirova <101700968+VeronicaBirova@users.noreply.github.com>
2023-05-31 08:23:36 +0300

    Add files via upload

---
+++ tsp (salesman)/tsp_4.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+long double distance(long double x1, long double x2, long double y1, long double y2){
+    long double a,b;
+    a=x1-x2;
+    b=y1-y2;
+    return sqrt(a*a+b*b);
+    //находим расстояние между двумя городами (с координатами (x1,y1),(x2,y2) по т.Пифагора
+}
+
+int main(){
+    int N, i, j, k, t;
+    cin>>N; //количество городов
+
+    long double min, dist, a, b;
+    vector<long double> x(N);
+    vector<long double> y(N);
+    vector<int> cities(N);
+    long double route_len=0; //длина пути
+
+    for(i=0; i<N; i++)
+        cin>>x[i]>>y[i];
+
+    for(i=0; i<N; i++)
+        cities[i]=i;
+    //последовательность посещения городов изначально берется из файла с входными данными
+
+    for(i=1; i<N; i++){
+        min=100000000;
+        k=i;
+
+        for(j=i; j<N; j++){
+            //i-1 - номер города, в котором мы находимся, j - номер просматриваемого города
+            dist=distance(x[cities[i-1]], x[cities[j]], y[cities[i-1]], y[cities[j]]);
+            //просматриваем все оставшиеся города и находим, до какого от текущего города минимальное расстояние
+            //(если минимальных путей несколько, берем первый попавшийся)
+            if(dist<min){
+                min=dist;
+                k=j;
+            }
+        }
+
+        //когда нашли минимальный путь, перемещаем этот город к городу, в котором мы находимся
+        t=cities[i];
+        cities[i]=cities[k];
+        cities[k]=t;
+        route_len+=min;
+    }
+
+    //возвращаемся в город, из которого стартовали
+    route_len+=distance(x[cities[N-1]], x[cities[0]], y[cities[N - 1]], y[cities[0]]);
+
+    /*for(i=0; i<N; i++)
+        cout<<cities[i]<<' ';
+    cout<<endl;*/
+
+    //избавляемся от экспоненты в выводе (точность - 2 знака после запятой)
+    cout<<fixed<<setprecision(2)<<route_len<<endl;
+
+    return 0;
+}
---
+++ tsp (salesman)/tsp_shuffle.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+
+#define START_LEN 29998.13//минимальный путь, полученный простым алгоритмом, который мы хотим уменьшить
+#define ITER 100000 //количество итераций, на протяжении которых мы "перемешиваем" содержание и ищем лучший минимальный путь
+
+long double distance(long double x1, long double x2, long double y1, long double y2){
+    long double a,b;
+    a=x1-x2;
+    b=y1-y2;
+    return sqrt(a*a+b*b);
+}
+
+int main(){
+    unsigned seed=std::chrono::system_clock::now().time_since_epoch().count();
+
+    int N, i, j, k, t, g;
+    cin>>N;
+
+    long double min, dist, a, b, sol;
+    vector<long double> x(N);
+    vector<long double> y(N);
+    vector<int> cities(N);
+    long double route_len, global_route_len=100000000;
+
+    for(i=0; i<N; i++){
+        cin>>x[i]>>y[i];
+        //cout<<x[i]<<' '<<y[i]<<' '<<endl;
+    }
+
+    for(i=0; i<N; i++)
+        cities[i]=i;
+
+    for(g=0; g<ITER; g++){
+        route_len=0;
+        shuffle (cities.begin(), cities.end(), default_random_engine(seed));
+
+        for(i=1; i<N; i++){
+            min=100000000;
+            k=i;
+
+            for(j=i; j<N; j++){
+                dist=distance(x[cities[i - 1]], x[cities[j]], y[cities[i - 1]], y[cities[j]]);
+                if(dist<min){
+                    min=dist;
+                    k=j;
+                }
+            }
+
+            t=cities[i];
+            cities[i]=cities[k];
+            cities[k]=t;
+            route_len+=min;
+        }
+
+        route_len+=distance(x[cities[N-1]], x[cities[0]], y[cities[N - 1]], y[cities[0]]);
+
+        if((route_len<START_LEN)&&(route_len<global_route_len))
+            cout<<fixed<<setprecision(2)<<route_len<<endl;
+        if (route_len<global_route_len)
+            global_route_len=route_len;
+
+    }
+    /*for(i=0; i<N; i++)
+        cout<<cities[i]<<' ';
+    cout<<endl;*/
+
+    cout<<fixed<<setprecision(2)<<global_route_len<<endl;
+
+    return 0;
+}
