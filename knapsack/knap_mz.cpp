274a5aeb2e4ccd2c5ffb6b96b1f60923a132fadf
VeronicaBirova <101700968+VeronicaBirova@users.noreply.github.com>
2023-05-31 22:49:24 +0300

    Update knap_mz.cpp

--- knapsack/knap_mz.cpp
+++ knapsack/knap_mz.cpp
@@ -4,35 +4,38 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-	char* filename = argv[1];
-    setlocale(LC_ALL, "Russian");
+int main(int argc, char** argv){
+	char* filename=argv[1];
+    	setlocale(LC_ALL, "Russian");
 	ifstream fin(filename);
 	ofstream data;
 	data.open("data.dzn");
+
 	long N, W, vt, wt, i, j;
-	fin >> N >> W;
-	data << "N = " << N << ';' << endl;
-	data << "W = " << W << ';' << endl;
+	fin>>N>>W;
+	data<<"N="<<N<<";"<<endl;
+	data<<"W="<<W<<";"<<endl;
 	vector <long> values(N);
 	vector <long> weights(N);
-	for (i = 0; i < N; i++) {
-		fin >> vt >> wt;
-		values[i] = vt;
-		weights[i] = wt;
+
+	for(i=0; i<N; i++) {
+		fin>>vt>>wt;
+		values[i]=vt;
+		weights[i]=wt;
 	}
-	data << "values = [";
-	for (i = 0; i < N; i++) {
-		data << values[i] << ", ";
-	}
-	data << "];";
-	data << endl;
-	data << "weights = [";
-	for (i = 0; i < N; i++) {
-		data << weights[i] << ", ";
-	}
-	data<<"];";
-	data<<endl;
+
+	data<<"values=[";
+	for(i=0; i<N-1; i++)
+		data<<values[i]<<", ";
+        data<<values[i];
+	data<<"];"<<endl;
+
+	data<<"weights=[";
+	for(i=0; i<N-1; i++)
+		data<<weights[i]<<", ";
+    	data<<weights[i];
+	data<<"];"<<endl;
+
 	data.close();
 	fin.close();
 	system("minizinc --solver Gecode knapsack.mzn data.dzn");
