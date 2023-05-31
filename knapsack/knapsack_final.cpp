407a3d3673c566e583c225c3a62f38ca047febba
VeronicaBirova <101700968+VeronicaBirova@users.noreply.github.com>
2023-05-31 22:52:26 +0300

    Update knapsack_final.cpp

--- knapsack/knapsack_final.cpp
+++ knapsack/knapsack_final.cpp
@@ -4,9 +4,9 @@
 #include <fstream>
 using namespace std;
 
-long W; //ðàçìåð ðþêçàêà
-vector<long> c; //ñòîèìîñòü êàæäîãî ïðåäìåòà
-vector<long> v; //âåñ êàæäîãî ïðåäìåòà
+long W; //размер рюкзака
+vector<long> c; //стоимость каждого предмета
+vector<long> v; //вес каждого предмета
 
 int load_data(char* fn){
     int n;
@@ -22,28 +22,26 @@ int load_data(char* fn){
     return 0;
 }
 
-//âîçâðàùàåò ìàêñèìàëüíóþ ñòîèìîñòü ïðåäìåòîâ, êîòîðûå ìîãóò áûòü óëîæåíû â ðþêçàê ðàçìåðîì W
+//возвращает максимальную стоимость предметов, которые могут быть уложены в рюкзак размером W
 int knapsack(int W, const vector<long>& v, const vector<long>& c){
-    int n = v.size();
-    vector<vector<long>> dp(n + 1, vector<long>(W + 1, 0));
+    int n=v.size();
+    vector<vector<long>> dp(n+1, vector<long>(W+1, 0));
 
-    for (int i=1; i<=n; ++i){
-        for (int w=0; w<=W; ++w){
-            if (v[i-1]<=w)
+    for(int i=1; i<=n; i++)
+        for(int w=0; w<=W; w++)
+            if(v[i-1]<=w)
                 dp[i][w]=max(dp[i-1][w], dp[i-1][w-v[i-1]]+c[i-1]);
             else
                 dp[i][w]=dp[i-1][w];
-        }
-    }
 
-    /*îòëàäî÷íàÿ ïå÷àòü
-    for(int i=1; i<=n; ++i){
-        cout << endl;
-        for(int j=0; j<=W; ++j)
-            cout << dp[i][j]<<" ";
+    //отладочная печать
+    /*for(int i=1; i<=n; i++){
+        cout<<endl;
+        for(int j=0; j<=W; j++)
+            cout<<dp[i][j]<<" ";
 
     }*/
-    cout << endl;
+    cout<<endl;
 
     return dp[n][W];
 }
@@ -54,6 +52,6 @@ int main(int argc, char** argv){
     load_data(filename);
 
     long maxValue=knapsack(W, v, c);
-    cout<<"Ìàêñèìàëüíàÿ ñòîèìîñòü ïðåäìåòîâ: "<<maxValue<<endl;
+    cout<<"Max item value: "<<maxValue<<endl;
     return 0;
 }
